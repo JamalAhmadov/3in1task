8cfb9548d9043d80fccfbff009ab4a79b7d4b212
Camal Əhmədov <146194995+JamalAhmadov@users.noreply.github.com>
2024-01-07 10:58:56 +0400

    Add files via upload
    2 ve 3 tma, 1 deyil

---
+++ ESSENCE/assets/js/index.js
@@ -0,0 +1,63 @@
+let container = document.getElementById("container");
+let loadBtn = document.getElementById("loadBtn");
+let search = document.getElementById("search");
+
+let page = 1;
+let limit = 5;
+
+const renderData = async () => {
+  const res = await fetch(
+    `https://65745c66f941bda3f2afa6af.mockapi.io/products?page=${page}&limit=${limit}`
+  );
+  const data = await res.json();
+  db = data;
+  db.forEach((item) => {
+    let card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+        <img src="${item.image}" alt=""> 
+        <div class="title">
+        <h6>${item.name}</h6>
+        <p>$: ${item.price}</p>
+
+        </div>
+        <button onclick="addToBasket(${item.id})">$ ${item.price}</button>
+        `;
+    container.append(card);
+  });
+  page++;
+};
+
+loadBtn.addEventListener("click", renderData);
+renderData();
+
+const searchByName = async (name) => {
+  const res = await fetch(
+    `https://65745c66f941bda3f2afa6af.mockapi.io/products`
+  );
+  const data = await res.json();
+  let flteddata = data.filter((item) => item.name.toLowerCase().includes(name));
+  container.innerHTML = " ";
+  flteddata.forEach((item) => {
+    let card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+        <img src="${item.image}" alt="">
+        <h6>${item.name}</h6>
+        <button onclick="addToBasket(${item.id})">$ ${item.price}</button>
+        `;
+    container.append(card);
+  });
+};
+search.addEventListener("input", (e) => {
+  searchByName(e.target.value);
+});
+
+
+function addToBasket(id) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(db.find((item) => item.id == id));
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+
